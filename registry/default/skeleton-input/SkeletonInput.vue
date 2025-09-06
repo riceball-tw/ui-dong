86a42b075e1796349ef04f6fcbf292c3113409c3
riceball <42161586+riceball-tw@users.noreply.github.com>
2025-09-06 10:19:12 +0800

    feat: Add skeletonInput & SwitchCard into registry

---
+++ registry/default/skeleton-input/SkeletonInput.vue
@@ -0,0 +1,11 @@
+
+<script setup lang="ts">
+import { Skeleton } from '@/components/ui/skeleton';
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 w-full">
+    <Skeleton style="width: 100px; height: 14px;" class="rounded-full" />
+    <Skeleton class="w-full h-9 rounded-md" />
+  </div>
+</template>
---
+++ registry/default/switch-card/SwitchCard.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import Label from '@/components/ui/label/Label.vue'
+import Switch from '@/components/ui/switch/Switch.vue'
+
+interface Props {
+  modelValue: boolean
+  label?: string
+  sublabel?: string
+  description?: string
+  id?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  label: 'Label',
+  sublabel: '(Sublabel)',
+  description: 'A short description goes here.',
+  id: 'switch-card'
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void
+}>()
+
+const checked = computed({
+  get: () => props.modelValue,
+  set: (val: boolean) => emit('update:modelValue', val)
+})
+</script>
+
+<template>
+  <div
+    class="relative flex w-full items-start gap-2 rounded-lg border border-input p-4 shadow-xs shadow-black/[.04] has-data-[state=checked]:border-ring"
+  >
+    <Switch
+      :id="props.id"
+      v-model="checked"
+      class="order-1 h-4 w-6 after:absolute after:inset-0 [&_span]:size-3 data-[state=checked]:[&_span]:translate-x-2 data-[state=checked]:[&_span]:rtl:-translate-x-2"
+      :aria-describedby="`${props.id}-description`"
+    />
+    <div class="grid grow gap-2">
+      <Label :for="props.id">
+        {{ props.label }}
+        <span
+          v-if="props.sublabel"
+          class="text-xs font-normal leading-[inherit] text-muted-foreground"
+        >
+          {{ props.sublabel }}
+        </span>
+      </Label>
+      <p
+        v-if="props.description"
+        :id="`${props.id}-description`"
+        class="text-xs text-muted-foreground"
+      >
+        {{ props.description }}
+      </p>
+    </div>
+  </div>
+</template>
